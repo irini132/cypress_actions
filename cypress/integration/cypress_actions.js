5016aaf7cfed0722936604475bf545871e7501d6
Irina1302 <imikhailenko92@gmail.com>
2021-02-10 23:30:00 +0200

    third  hw

--- cypress/integration/cypress_actions.js
+++ cypress/integration/cypress_actions.js
@@ -16,3 +16,22 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+it('Fulfill the form', () => {
+  cy.visit('/automation-practice-form')
+  cy.get('[placeholder="First Name"]').type('Mimimmimim')
+  cy.get('[placeholder="Last Name"]').type('Mryamrya')
+  cy.get('[placeholder="name@example.com"]').type('murmur@gmail.com')
+  cy.get('[for="gender-radio-2"]').click()
+  cy.get('[placeholder="Mobile Number"]').type('04357456312')
+
+  // cy.get('#dateOfBirthInput').type('{selectAll}14 Feb 2001{enter}')
+
+  // cy.get('#dateOfBirthInput').click()
+  // cy.get('.react-datepicker__year-select').contains('1990').click({ force: true })
+  // cy.get('.react-datepicker__month-select').contains('February').click({ force: true })
+  // cy.get('.react-datepicker__day.react-datepicker__day--009').click({ force: true })
+  cy.get('.subjects-auto-complete__placeholder.css-1wa3eu0-placeholder').type('ch', { delay: 1000 });
+  cy.contains('.subjects-auto-complete__control.css-yk16xz-control', 'Chemistry').select()
+}
+)
