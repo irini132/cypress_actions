64c9a54b6a8ecd732decf8cb05dcd7362e174ba0
Irina1302 <imikhailenko92@gmail.com>
2021-02-11 16:15:57 +0200

    third  hw1

--- cypress/integration/cypress_actions.js
+++ cypress/integration/cypress_actions.js
@@ -18,20 +18,35 @@
 // https://demoqa.com/webtables
 
 it('Fulfill the form', () => {
-  cy.visit('/automation-practice-form')
+  cy.visit('/automation-practice-form');
   cy.get('[placeholder="First Name"]').type('Mimimmimim')
-  cy.get('[placeholder="Last Name"]').type('Mryamrya')
-  cy.get('[placeholder="name@example.com"]').type('murmur@gmail.com')
-  cy.get('[for="gender-radio-2"]').click()
-  cy.get('[placeholder="Mobile Number"]').type('04357456312')
+  cy.get('[placeholder="Last Name"]').type('Mryamrya');
+  cy.get('[placeholder="name@example.com"]').type('murmur@gmail.com');
+  cy.get('[for="gender-radio-2"]').click();
+  cy.get('[placeholder="Mobile Number"]').type('04357456312');
 
-  // cy.get('#dateOfBirthInput').type('{selectAll}14 Feb 2001{enter}')
+  // cy.get('#dateOfBirthInput').then((year) => {
+  //   //cy.get(year).click()
+  //   cy.get('.react-datepicker__year-select').select('1990')
+  //   cy.get('.react-datepicker__month-select').select('February')
+  //   cy.get('.react-datepicker__day.react-datepicker__day--010')
+  // });
+  
+  cy.get('#dateOfBirthInput').type('{selectall}13 Feb 1992{enter}')
 
-  // cy.get('#dateOfBirthInput').click()
-  // cy.get('.react-datepicker__year-select').contains('1990').click({ force: true })
-  // cy.get('.react-datepicker__month-select').contains('February').click({ force: true })
-  // cy.get('.react-datepicker__day.react-datepicker__day--009').click({ force: true })
-  cy.get('.subjects-auto-complete__placeholder.css-1wa3eu0-placeholder').type('ch', { delay: 1000 });
-  cy.contains('.subjects-auto-complete__control.css-yk16xz-control', 'Chemistry').select()
+  cy.get('#subjectsContainer').type('Ch{downArrow}{enter}')
+  
+
+//   cy.get('.subjects-auto-complete__control.css-yk16xz-control').type('Chemistry{enter}')
+  
+  cy.contains('.custom-control', 'Sport')
+    .find('[value="1"]').click({force: true})
+
+  cy.get('[placeholder="Current Address"]').type('0435ewr34yrejvio2e84qihdvc fe8y23ehsv n uerweha7456312');
+
+  // cy.get('.css-1wa3eu0-placeholder').contains('Select State').click()
+  cy.get('#state').contains('Select State').type('Haryana{enter}')
+  cy.get('#city').contains('Select City').type('Karnal{enter}')
+  cy.get('#submit').click()
 }
 )
